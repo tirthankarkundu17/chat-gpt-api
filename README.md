9b054aaee7bb7c04dc64348a41d04f39ae7d95df
Tirthankar Kundu <tirthankar.learning@gmail.com>
2023-01-14 02:36:14 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # chat-gpt-api
 Serverless Go based Chat GPT API
+
+### Local development and Testing
+
+**Invoking function locally through local API Gateway**
+
+Create a new Chat GPT Token from [here](https://beta.openai.com/account/api-keys)\
+Update the <CHAT_GPT_TOKEN> in template.yaml
+```
+Environment:
+    Variables:
+        CHAT_GPT_TOKEN: <CHAT_GPT_TOKEN>
+```
+
+Then run below command 
+```
+sam build
+sam local start-api
+```
+
+If the previous command ran successfully you should now be able to hit the following local endpoint to invoke your function `http://localhost:3000/converse`
+
+
+## Packaging and deployment
+
+Build the app and then deploy it to AWS
+```
+sam build
+sam deploy --guided
+```
+
+# See it in Action
+
+After deployment try it using postman 
+<img src="./docs/sample.png">
---
+++ pkg/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type TextCompletionRequest struct {
+	Model            string  `json:"model"`
+	Prompt           string  `json:"prompt"`
+	Temperature      float64 `json:"temperature"`
+	MaxTokens        int     `json:"max_tokens"`
+	TopP             float64 `json:"top_p"`
+	FrequencyPenalty float64 `json:"frequency_penalty"`
+	PresencePenalty  float64 `json:"presence_penalty"`
+}
+
+type Choice struct {
+	Text         string      `json:"text"`
+	Index        int         `json:"index"`
+	Logprobs     interface{} `json:"logprobs"`
+	FinishReason string      `json:"finish_reason"`
+}
+
+type TextCompletionResponse struct {
+	ID      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int      `json:"created"`
+	Model   string   `json:"model"`
+	Choices []Choice `json:"choices"`
+	Usage   struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+}
+
+type APIResponse struct {
+	Status string    `json:"status"`
+	Data   *[]Choice `json:"data"`
+}
+
+type APIRequest struct {
+	Prompt string `json:"prompt"`
+}
+
+var (
+	// ChatGPTHTTPAddress  Address
+	ChatGPTHTTPAddress = "https://api.openai.com/v1/completions"
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("non 200 response found")
+
+	// Bearer Token
+	BearerToken = os.Getenv("CHAT_GPT_TOKEN")
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var apiRequest APIRequest
+	if err := json.Unmarshal([]byte(request.Body), &apiRequest); err != nil { // Parse []byte to go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	httpReq := TextCompletionRequest{
+		Model:            "text-davinci-003",
+		Prompt:           apiRequest.Prompt,
+		Temperature:      0.7,
+		MaxTokens:        100,
+		TopP:             1.0,
+		FrequencyPenalty: 0.0,
+		PresencePenalty:  0.0,
+	}
+	jsonValue, _ := json.Marshal(httpReq)
+
+	bearer := "Bearer " + BearerToken
+
+	req, err := http.NewRequest("POST", ChatGPTHTTPAddress, bytes.NewBuffer(jsonValue))
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	req.Header.Set("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	var textCompletionResponse TextCompletionResponse
+	if err := json.Unmarshal(body, &textCompletionResponse); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	var choices = textCompletionResponse.Choices
+
+	response := APIResponse{
+		Status: "Success",
+		Data:   &choices,
+	}
+
+	jsonBytes, _ := json.Marshal(response)
+	rstr := fmt.Sprint(string(jsonBytes))
+
+	return events.APIGatewayProxyResponse{
+		Body:       rstr,
+		StatusCode: 200,
+	}, nil
+}
+
+func PrettyPrint(textCompletionResponse TextCompletionResponse) {
+	panic("unimplemented")
+}
+
+func main() {
+	lambda.Start(handler)
+}
